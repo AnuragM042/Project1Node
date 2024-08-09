fdd959ebe966590f02227e363f27a3cd7b08a211
Anurag mishra <anurazzz.mishra098@gmail.com>
2024-08-09 19:03:10 +0530

    user-modal , jwt,andbcrypt basics

---
+++ models/user.modal.js
@@ -0,0 +1,102 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"; // bearer token(key)
+import bcrypt from "bcrypt"; // for encrypt and decrypt
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // helps in searching but can lag the db with space
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trime: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      // unique: true,
+      // lowercase: true,
+      trime: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      // indicates an array (multiple items)
+      {
+        // indicates an object
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is requirec"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// pre is a hook
+// dont use arrow function as there is no "this" context
+// use async as we are connecting to db and next for passing flag forward
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // if password not modified then directly next
+  // if password is modified(updated)  then bcrypt it
+  this.password = bcrypt.hash(this.password, 10); // 10 salts/hash round
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  // password is user typing password and this.password is saved users password
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAcessToken = function () {
+  return jwt.sign(
+    // to generate a token
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    // to generate a token
+    {
+      _id: this._id,
+    
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
